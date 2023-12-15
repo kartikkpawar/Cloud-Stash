1b0ac432ac649a525d43226b9fc1052de4935113
kartikkpawar <knpwar789@gmail.com>
2023-12-15 22:50:35 +0530

    log removed

--- firebase.ts
+++ firebase.ts
@@ -11,8 +11,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
-console.log(firebaseConfig);
-
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
