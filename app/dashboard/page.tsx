06510b9389f65696172779a9372af4aed6744206
kartikkpawar <knpwar789@gmail.com>
2023-12-15 22:51:10 +0530

    Firebase loaduser data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,35 @@
 import Dropzone from "@/components/Dropzone";
+import TableWrapper from "@/components/table/TableWrapper";
+import { db } from "@/firebase";
+import { FileType } from "@/type";
 import { auth } from "@clerk/nextjs";
+import { collection, getDocs } from "firebase/firestore";
 import React from "react";
 
-function DashBoard() {
+async function DashBoard() {
   const { userId } = auth();
+
+  const docResults = await getDocs(collection(db, "users", userId!, "files"));
+
+  const skeletonFiles: FileType[] = docResults.docs.map((doc) => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    fullname: doc.data().fullName,
+    timeStamp: new Date(doc.data().timestamp?.seconds * 1000) || null,
+    downloadUrl: doc.data().downloadUrl,
+    type: doc.data().type,
+    size: doc.data().size,
+  }));
+
   return (
-    <div>
+    <div className="border-t">
       <Dropzone />
+      <section className="container space-y-5">
+        <h2>All Files</h2>
+        <div>
+          <TableWrapper skeletonFiles={skeletonFiles} />
+        </div>
+      </section>
     </div>
   );
 }
