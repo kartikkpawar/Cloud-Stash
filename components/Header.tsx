fd35107e3a0fc1971cd2cb13aab1d2e0abbf3fd6
kartikkpawar <knpwar789@gmail.com>
2023-12-15 14:39:12 +0530

    Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Themetoggler from "./ThemeToggler";
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+      <Link href="/" className="flex items-center space-x-2">
+        <div className="bg-[#0160FE]">
+          <Image
+            src="https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png"
+            alt="logo"
+            className="invert"
+            height={50}
+            width={50}
+          />
+        </div>
+        <h1 className="font-bold text-xl">Dropbox</h1>
+      </Link>
+      <div className="flex justify-between px-5 space-x-2">
+        <Themetoggler />
+        <UserButton afterSignOutUrl="/" />
+        <SignedOut>
+          <SignInButton afterSignInUrl="/dashboard" mode="modal" />
+        </SignedOut>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
