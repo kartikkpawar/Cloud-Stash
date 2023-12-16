6d8eb6173e3afa86bc9b042a7b4aa309f3bb63f8
kartikkpawar <knpwar789@gmail.com>
2023-12-16 11:49:05 +0530

    Skeleton Loading added

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -1,14 +1,81 @@
+"use client";
 import { FileType } from "@/type";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { DataTable } from "./Table";
 import { columns } from "./Columns";
+import { useUser } from "@clerk/nextjs";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase";
+import { Skeleton } from "@/components/ui/skeleton";
 
 function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
+  const { user } = useUser();
+
+  const [intialFiles, setIntialFiles] = useState<FileType[]>([]);
+  const [sort, setSort] = useState<"asc" | "desc">("desc");
+
+  const [docs, loading, error] = useCollection(
+    user &&
+      query(
+        collection(db, "users", user.id, "files"),
+        orderBy("timestamp", sort)
+      )
+  );
+
+  useEffect(() => {
+    if (!docs) return;
+    const files: FileType[] = docs.docs.map((doc) => ({
+      id: doc.id,
+      filename: doc.data().filename || doc.id,
+      fullname: doc.data().fullName,
+      timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+      downloadUrl: doc.data().downloadUrl,
+      type: doc.data().type,
+      size: doc.data().size,
+    }));
+    setIntialFiles(files);
+  }, [docs]);
+
+  if (docs?.docs.length === undefined) {
+    return (
+      <div className="flex flex-col">
+        <Button className="ml-auto w-36 h-10 mb-5" variant={"outline"}>
+          <Skeleton className="h-5 w-full" />
+        </Button>
+        <div className="border rounded-lg">
+          <div className="border-b h-max">
+            {skeletonFiles.map((file) => (
+              <div
+                key={file.id}
+                className="flex items-center space-x-4 p-5 w-full"
+              >
+                <Skeleton className="h-12 w-12" />
+                <Skeleton className="h-12 w-full" />
+              </div>
+            ))}
+            {skeletonFiles.length === 0 && (
+              <div className="flex items-center space-x-4 p-5 w-full">
+                <Skeleton className="h-12 w-12" />
+                <Skeleton className="h-12 w-full" />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <Button>Sort By...</Button>
-      <DataTable data={skeletonFiles} columns={columns} />
+    <div className="flex flex-col space-y-5 pb-10">
+      <Button
+        onClick={() => setSort(sort === "desc" ? "asc" : "desc")}
+        className="ml-auto w-fit"
+      >
+        Sort By {sort === "desc" ? "Newest" : "Oldest"}
+      </Button>
+      <DataTable data={intialFiles} columns={columns} />
     </div>
   );
 }
